a61e6ae4268316c822761fe1cbb406c78ebf7324
matsushita <matsushita@tierline.com>
2015-06-06 17:48:51 +0900

    Signed-off-by: matsushita <matsushita@tierline.com>

---
+++ project/Build.scala
@@ -0,0 +1,106 @@
+import sbt._
+import Keys._
+import org.scalatra.sbt._
+import org.scalatra.sbt.PluginKeys._
+import org.scalajs.sbtplugin.ScalaJSPlugin
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
+import com.earldouglas.xsbtwebplugin.PluginKeys.webappResources
+import java.io.{File, FileOutputStream}
+import java.nio.file._
+
+object ScalaWUIBuild extends Build {
+  val Organization = "com.tierline"
+  val Name = "scalajs-react-crud"
+  val Version = "0.1.0-SNAPSHOT"
+  val ScalaVersion = "2.11.6"
+  val ScalatraVersion = "2.3.0"
+  val Resolvers = Seq(Resolver.sonatypeRepo("public"),    
+    "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
+    "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
+  )
+
+  lazy val shared = project.in(file("./shared")).settings(
+    scalaVersion := ScalaVersion
+  )
+  
+//  resolvers in ThisBuild += Resolvers
+
+  lazy val client = Project(
+    "client",
+    file("client"),
+    settings = Seq(
+      version := Version,
+      scalaVersion := ScalaVersion,
+      resolvers in ThisBuild ++= Resolvers,
+      libraryDependencies ++= Seq(
+        "com.lihaoyi" %%% "autowire" % "0.2.5",
+        "com.lihaoyi" %%% "upickle" % "0.2.7",
+        "com.lihaoyi" %%% "scalatags" % "0.5.2",
+        "com.lihaoyi" %%% "scalarx" % "0.2.8",
+        "fr.iscpif" %%% "scaladget" % "0.5.0-SNAPSHOT",
+        "org.scala-js" %%% "scalajs-dom" % "0.8.0"
+      )
+    )
+  ).dependsOn(shared) enablePlugins (ScalaJSPlugin)
+
+  lazy val server = Project(
+    "server",
+    file("server"),
+    settings = ScalatraPlugin.scalatraWithJRebel ++ Seq(
+      organization := Organization,
+      name := Name,
+      version := Version,
+      scalaVersion := ScalaVersion,
+      resolvers ++= Resolvers,
+      webappResources in Compile := Seq(target.value / "webapp"),
+      libraryDependencies ++= Seq(
+        "com.lihaoyi" %% "autowire" % "0.2.5",
+        "com.lihaoyi" %% "upickle" % "0.2.7",
+        "com.lihaoyi" %% "scalatags" % "0.5.2",
+        "org.scalatra" %% "scalatra" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "ch.qos.logback" % "logback-classic" % "1.0.12" % "runtime",
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.17.v20150415" % "container",
+        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test"
+      )
+    )
+  ).dependsOn(shared)
+
+  lazy val go = taskKey[Unit]("go")
+
+  lazy val bootstrap = Project(
+    "bootstrap",
+    file("target/bootstrap"),
+    settings = Seq(
+      version := Version,
+      scalaVersion := ScalaVersion,
+      (go <<= (fullOptJS in client in Compile, resourceDirectory in client in Compile, target in server in Compile) map { (ct, r, st) =>
+        copy(ct, r, new File(st,"webapp"))
+      }
+        )
+    )
+  ) dependsOn(client, server)
+
+
+  private def copy(clientTarget: Attributed[File], resources: File, webappServerTarget: File) = {
+    clientTarget.map { ct =>
+      recursiveCopy(new File(resources, "webapp"), webappServerTarget)
+      recursiveCopy(ct, new File(webappServerTarget, "js/" + ct.getName))
+    }
+  }
+
+  private def recursiveCopy(from: File, to: File): Unit = {
+    if (from.isDirectory) {
+      to.mkdirs()
+      for {
+        f ← from.listFiles()
+      } recursiveCopy(f, new File(to, f.getName))
+    }
+    else if (!to.exists() || from.lastModified() > to.lastModified) {
+      println(s"Copy file $from to $to ")
+      from.getParentFile.mkdirs
+      IO.copyFile(from, to, preserveLastModified = true)
+    }
+  }
+
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,8 @@
+//resolvers += "Typesafe repository" at
+  //"http://repo.typesafe.com/typesafe/releases/"
+
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.3")
+
+addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.3.5")
+
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "4.0.0-RC2")
